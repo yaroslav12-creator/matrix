0a8f64e6e6d02956800b5b735a26c04b7410c97a
Yaroslav Shostak <yaroslavshostak4@gmail.com>
2022-03-14 10:06:13 +0200

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -4,10 +4,10 @@ const ctx = canvas.getContext("2d");
 let cw = window.innerWidth;
 let ch = window.innerHeight;
 
-let charArr = ['0', '1', 'P', 'G'];
+let charArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-let maxCharCount = 100;
-let fontSize = 15;
+let maxCharCount = 200;
+let fontSize = 18;
 let fallingCharArr = [];
 let maxColumns = cw / fontSize;
 canvas.width = cw;
@@ -25,8 +25,8 @@ class FallingChar {
     draw(ctx) {
         this.value = charArr[Math.floor(Math.random() * (charArr.length - 1))]
             .toUpperCase();
-        this.speed = Math.random() * fontSize * 3/4 + fontSize * 3/4;
-        ctx.fillStyle = "rgba(0,255,0)";
+        this.speed = Math.random() * fontSize * 3/6 + fontSize * 3/6;
+        ctx.fillStyle = "rgba(166, 10, 10, 1)";
         ctx.font = fontSize + "px san-serif";
         ctx.fillText(this.value, this.x, this.y);
         this.y += this.speed;
@@ -34,7 +34,7 @@ class FallingChar {
         if(this.y > ch) {
             this.y = Math.random() * ch / 2 - 50;
             this.x = Math.floor(Math.random() * maxColumns) * fontSize;
-            this.speed = - Math.random() * fontSize * 3/4 + fontSize * 3/4;
+            this.speed = Math.random() * fontSize * 3/7 + fontSize * 3/7;
         }
     }
 }
@@ -47,7 +47,7 @@ let update = () => {
             );
         fallingCharArr.push(fallingChar);
     }
-    ctx.fillStyle = "rgba(0,0,0,0.05)";
+    ctx.fillStyle = "rgba(0,0,0,0.03)";
     ctx.fillRect(0, 0, cw, ch);
     for(let i = 0; i < fallingCharArr.length && frames % 2 == 0; i++) {
         fallingCharArr[i].draw(ctx);
